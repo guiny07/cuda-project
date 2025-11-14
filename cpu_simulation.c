d8264e10c8151153c1e5651a3434c85cad5744b3
Guilherme Henrique <gui.henriquegs04@gmail.com>
2025-11-14 14:35:35 -0300

    Finished cpu version

--- cpu_simulation.c
+++ cpu_simulation.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 {
     if(argc < 2)
     {
-        perror("You most provide a .txt file.");
+        perror("You must provide a .txt file.");
         return 1;
     }
 
